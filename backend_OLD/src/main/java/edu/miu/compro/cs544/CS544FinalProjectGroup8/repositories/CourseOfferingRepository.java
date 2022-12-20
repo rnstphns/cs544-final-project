43d5babd0848f58fb2d0e4e4dd0f84c1c8d2927d
rnstphns <97855804+rnstphns@users.noreply.github.com>
2022-12-20 14:05:22 -0600

    remove backend-old

--- backend_OLD/src/main/java/edu/miu/compro/cs544/CS544FinalProjectGroup8/repositories/CourseOfferingRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.miu.compro.cs544.CS544FinalProjectGroup8.repositories;
-
-import edu.miu.compro.cs544.CS544FinalProjectGroup8.domain.CourseOffering;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseOfferingRepository extends JpaRepository<CourseOffering, Long> {
-}
--- backend_OLD/src/main/java/edu/miu/compro/cs544/CS544FinalProjectGroup8/repositories/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.miu.compro.cs544.CS544FinalProjectGroup8.repositories;
-
-import edu.miu.compro.cs544.CS544FinalProjectGroup8.domain.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
