3cd44b35e40e5d8b2dfc8dee6632a31052d49726
rnstphns <97855804+rnstphns@users.noreply.github.com>
2022-12-20 11:49:33 -0600

    remove old backend module

--- backend_OLD/src/main/java/edu/miu/compro/cs544/CS544FinalProjectGroup8/repositories/RegistrationRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.miu.compro.cs544.CS544FinalProjectGroup8.repositories;
-
-import edu.miu.compro.cs544.CS544FinalProjectGroup8.domain.RegistrationRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistrationRequestRepository extends JpaRepository<RegistrationRequest, Long> {
-}
