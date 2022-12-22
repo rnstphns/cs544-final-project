99a29d52b576cc0082c241aed61be9e3e29e204a
rnstphns <97855804+rnstphns@users.noreply.github.com>
2022-12-22 15:36:18 -0600

    use case 6 created (get student's registrations)

--- backend/src/main/java/edu/miu/cs544/backend/web/StudentController.java
+++ backend/src/main/java/edu/miu/cs544/backend/web/StudentController.java
@@ -1,16 +1,19 @@
 package edu.miu.cs544.backend.web;
 
 
+import edu.miu.cs544.backend.domain.Registration;
 import edu.miu.cs544.backend.domain.RegistrationEvent;
 import edu.miu.cs544.backend.domain.Student;
 import edu.miu.cs544.backend.exceptions.DatabaseException;
 import edu.miu.cs544.backend.service.RegistrationEventService;
+import edu.miu.cs544.backend.service.RegistrationService;
 import edu.miu.cs544.backend.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -20,6 +23,9 @@ public class StudentController {
 private StudentService studentServiceImpl;
     @Autowired
     private RegistrationEventService registrationEventService;
+
+    @Autowired
+    private RegistrationService registrationService;
 
     @GetMapping("/registration-events/latest")
     public ResponseEntity<RegistrationEvent> getAllCourseOfferings(){
@@ -36,19 +42,25 @@ private StudentService studentServiceImpl;
     }
 
 }
-@DeleteMapping("/deletebyid/{id}")
+    @DeleteMapping("/deletebyid/{id}")
     public ResponseEntity<Void> deletestudent(@PathVariable Long id){
         studentServiceImpl.delete(id);
         return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
 }
 
-@PutMapping("/update/{id}")
+    @PutMapping("/update/{id}")
     public  ResponseEntity<?> updateStudent(@RequestBody Student student , @PathVariable Long id){
         return  ResponseEntity.ok(studentServiceImpl.update(id, student));
 }
-@GetMapping("/studentlist")
+    @GetMapping("/studentlist")
     public  ResponseEntity<List<Student>> getallstudents(){
         var allstudents= studentServiceImpl.findAll();
         return ResponseEntity.ok(allstudents);
 }
+
+    @GetMapping("{studentId}/registrations")
+    public ResponseEntity<Collection<Registration>> getRegistrationsByStudentId(@PathVariable Integer studentId){
+        Collection<Registration> registrations = registrationService.getRegistrationsByStudentId(studentId);
+        return new ResponseEntity<Collection<Registration>>(registrations, HttpStatus.OK);
+    }
 }
